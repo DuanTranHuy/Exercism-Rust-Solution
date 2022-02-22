8971e8e80bba4a81fa8631f1423bc97efc4231e3
Huy Duan Tran <duanthse03268@gmail.com>
2022-02-22 23:04:43 +0700

    Update second day

--- assembly-line/src/lib.rs
+++ assembly-line/src/lib.rs
@@ -1,8 +1,19 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+// This stub file contains items which aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+pub fn production_rate_per_hour(speed: u8) -> f64 {
+    let speed_f64 = f64::from(speed);
+    let car_per_hour_f64 = f64::from(221);
+
+    match speed {
+        0..=4 => return speed_f64 * car_per_hour_f64,
+        5..=8 => return speed_f64 * car_per_hour_f64 * 0.9 as f64,
+        _ => return speed_f64 * car_per_hour_f64 * 0.77 as f64,
     }
+}
+
+pub fn working_items_per_minute(speed: u8) -> u32 {
+    let product =  production_rate_per_hour(speed);
+    return (product / 60.0) as u32
 }
